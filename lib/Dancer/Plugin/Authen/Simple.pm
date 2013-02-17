01c95936790d8310946f1dece13127ebc20f7355
Andrew Grangaard <granny-github@ofb.net>
2013-02-17 02:11:03 -0800

    adds authen keyword

--- lib/Dancer/Plugin/Authen/Simple.pm
+++ lib/Dancer/Plugin/Authen/Simple.pm
@@ -1,5 +1,24 @@
 use strict;
 use warnings;
-package Dancer::Plugin::Authen::Simple;
 
-1;
+package Dancer::Plugin::Authen::Simple;
+use Dancer ':syntax';
+use Dancer::Plugin;
+use Module::Load;
+use Authen::Simple;
+
+sub authen
+{
+    my $conf     = plugin_settings();
+    my @adapters = ();
+    foreach my $adapter_name ( keys %{$settings} )
+    {
+        my $driver = "Authen::Simple::$adapter_name";
+        load $driver;
+        push @adapters, $driver->new( $settings->{$adapter_name} );
+    }
+    Authen::Simple->new(@adapters);
+}
+
+register authen => \&authen;
+register_plugin for_versions => [ 1, 2 ];
