4d9bd6da7d63318f84e3c766fcff38d3e22dae16
Andrew Grangaard <granny-github@ofb.net>
2013-02-17 10:01:32 -0800

    fixes Authen::Simple params, adds Documentation and test, boilerplate
    * adds ABSTRACT, SYNOPSIS and CONFIGURATION sections.
    * passes args to Authen::Simple::*->new() as hash rather than hashref
    * t/00_load.t -- simple load module test
    * fixes syntax errors, finishes replacement of $settings with $conf
    * fleshes out dist.ini
    * adds .gitignore for temporary files made by dist.in

--- lib/Dancer/Plugin/Authen/Simple.pm
+++ lib/Dancer/Plugin/Authen/Simple.pm
@@ -7,15 +7,90 @@ use Dancer::Plugin;
 use Module::Load;
 use Authen::Simple;
 
+#ABSTRACT: Easy Authentication for Dancer applications via Authen::Simple
+
+=head1 SYNOPSIS
+
+    use Dancer;
+    use Dancer::Plugins::Authen::Simple;
+
+    # calling the authen keyword will get you a Authen::Simple object
+    # e.g. authen->authenticate( $user, $pass)
+
+    hook 'before' => sub {
+        if ( !session('user') && request->path_info !~ m{^/login} )
+        {
+            var requested_path => request->path_info;
+            request->path_info('/login');
+        }
+    };
+    get '/login' => sub {
+        template 'login', { path => vars->{requested_path} };
+    };
+    post '/login' => sub {
+        if ( authen->authenticate( params->{user}, params->{pass} ) )
+        {
+            debug "Password correct";
+
+            # Logged in successfully
+            session user => $user;
+            redirect params->{path} || '/';
+        }
+        else
+        {
+            debug("Login failed - password incorrect for " . params->{user});
+            redirect '/login?failed=1';
+        }
+    };
+
+=cut
+
+=head1 CONFIGURATION
+
+Configuration details will be taken from your Dancer application config file.  Each sub-key of Authen::Simple will add an additional Authen::Simple:* module to the parent Authen::Simple object.
+
+See Authen::Simple for details on configuration options for each module.
+
+Example configuration for Authen::Simple::Kerberos, Authen::Simple::SMB and Authen::Simple::LDAP:
+
+    plugins:
+        "Authen::Simple":
+            Kerberos:
+                realm: 'REALM.EXAMPLE.COM'
+            SMB:
+                domain: 'DOMAIN'
+                pdc:    'PDC'
+            LDAP
+                host: 'ldap.example.com'
+                binddn: 'example_user'
+                bindpw: 'example_password'
+                basedn: 'ou=People,dc=example,dc=com'
+                filter: '(sAMAccountName=%s)'
+
+This is functionally equivalent to:
+
+    use Authen::Simple;
+    use Authen::Simple::Kerberos;
+    use Authen::Simple::SMB;
+    use Authen::Simple::LDAP;
+
+    my $authen = Authen::Simple->new(
+        Authen::Simple::Kerberos->new(realm => 'REALM.EXAMPLE.COM'),
+        Authen::Simple::SMB->new(domain => 'DOMAIN', pdc => 'PDC'),
+        Authen::Simple::LDAP->new(host => 'ldap.example.com, ... )
+    );
+
+=cut
+
 sub authen
 {
     my $conf     = plugin_settings();
     my @adapters = ();
-    foreach my $adapter_name ( keys %{$settings} )
+    foreach my $adapter_name ( keys %$conf )
     {
         my $driver = "Authen::Simple::$adapter_name";
         load $driver;
-        push @adapters, $driver->new( $settings->{$adapter_name} );
+        push @adapters, $driver->new( %{ $conf->{$adapter_name} } );
     }
     Authen::Simple->new(@adapters);
 }
